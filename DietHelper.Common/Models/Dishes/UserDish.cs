d4c41be01ca99ffa7f0861ab270f9a21f682adc1
GrinchenkoAnna <grinchenkoanna@inbox.ru>
2025-12-10 20:39:31 +0700

    Added new classes for users (not deleting old ones)
    Products have a basic form and a customized (user) form with customized nutrition facts. Dishes are assembled from user- or base products

---
+++ DietHelper.Common/Models/Dishes/UserDish.cs
@@ -0,0 +1,45 @@
+﻿using DietHelper.Common.Models.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DietHelper.Common.Models.Dishes
+{
+    public class UserDish
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+        public virtual User User { get; set; } = null!;
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public NutritionInfo NutritionFacts { get; set; } = new();
+
+        public bool IsDeleted { get; set; } = false;
+
+        public virtual ICollection<UserDishIngredient> Ingredients { get; set; } = new List<UserDishIngredient>();
+
+        [NotMapped]
+        public NutritionInfo DishNutrition => new()
+        {
+            Calories = Ingredients.Sum(i => i.CurrentNutrition.Calories),
+            Protein = Ingredients.Sum(i => i.CurrentNutrition.Protein),
+            Fat = Ingredients.Sum(i => i.CurrentNutrition.Fat),
+            Carbs = Ingredients.Sum(i => i.CurrentNutrition.Carbs)
+        };
+
+        public void UpdateNutritionFromIngredients()
+        {
+            NutritionFacts = DishNutrition;
+        }
+    }
+}
---
+++ DietHelper.Common/Models/Dishes/UserDishIngredient.cs
@@ -0,0 +1,55 @@
+﻿using DietHelper.Common.Models.Core;
+using DietHelper.Common.Models.Products;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DietHelper.Common.Models.Dishes
+{
+    public class UserDishIngredient
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public int UserDishId { get; set; }
+        public virtual UserDish Dish { get; set; } = null!;
+
+        [Required]
+        public int UserProductId { get; set; }
+        public virtual UserProduct Product { get; set; } = null!;
+
+        [Required]
+        public double Quantity { get; set; }
+
+        [NotMapped]
+        private double Factor => Quantity / 100;
+
+        public bool IsDeleted { get; set; } = false;        
+
+        [NotMapped]
+        public NutritionInfo CurrentNutrition
+        {
+            get
+            {
+                if (Product == null) return new NutritionInfo();
+
+                var factor = Quantity / 100;
+                var nutrition = Product.CustomNutrition;
+
+                return new NutritionInfo()
+                {
+                    Calories = nutrition.Calories * factor,
+                    Protein = nutrition.Protein * factor,
+                    Fat = nutrition.Fat * factor,
+                    Carbs = nutrition.Carbs * factor
+                };
+            }
+        }
+    }
+}
