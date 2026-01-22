146ca15a94947824fd0bc2c3ea7eff7b514e14a5
GrinchenkoAnna <grinchenkoanna@inbox.ru>
2026-01-23 02:12:18 +0700

    Update AuthViewModel.cs
    added methods
    - login
    - register
    - reset (to clean the form and all properties)

--- DietHelper/ViewModels/AuthViewModel.cs
+++ DietHelper/ViewModels/AuthViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DietHelper.Common.DTO;
 using DietHelper.Services;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,12 +16,107 @@ namespace DietHelper.ViewModels
         private readonly ApiService _apiService;
         private readonly INavigationService _navigationService;
 
+        [ObservableProperty]
+        private string userName = string.Empty;
+
+        [ObservableProperty]
+        private string password = string.Empty;
+
+        [ObservableProperty]
+        private string confirmPassword = string.Empty;
+
+        [ObservableProperty]
+        private string message = string.Empty;
+
         public AuthViewModel(ApiService apiService, INavigationService navigationService)
         {
             _apiService = apiService;
             _navigationService = navigationService;
 
-            
+            if (_apiService.IsAuthenticated)
+                _ = _navigationService.NavigateToMainAsync();
+        }
+
+        [RelayCommand]
+        private async Task Login()
+        {
+            Reset();
+
+            if (string.IsNullOrWhiteSpace(UserName) || string.IsNullOrWhiteSpace(Password))
+                Message = "Введите логин и пароль";
+
+            try
+            {
+                var result = await _apiService.LoginAsync(
+                    new LoginDto
+                    {
+                        UserName = UserName,
+                        Password = Password
+                    });
+                if (result!.IsSuccess)
+                {
+                    Message = "Авторизация прошла успешно";
+                    await _navigationService.NavigateToMainAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"[AuthViewModel]: {ex.Message}");
+                Message = "Ошибка авторизации";
+            }
+        }
+
+        private bool IsPasswordValid()
+        {
+            if (Password.Length >= 8 && Password.Length <= 50 && Password.Any(char.IsDigit)
+                && Password.Any(char.IsLower) && Password.Any(char.IsUpper))
+                return true;
+            return false;
+        }
+
+        [RelayCommand]
+        private async Task Register()
+        {
+            Reset();
+
+            if (string.IsNullOrWhiteSpace(UserName) || string.IsNullOrWhiteSpace(Password))
+                Message = "Введите логин и пароль";
+
+            if (!IsPasswordValid())
+                Message = "Пароль должен быть не менее 8 символов и не более 50, содержать хотя бы одну строчную и одну прописную букву, а также цифру";
+
+            if (string.IsNullOrWhiteSpace(ConfirmPassword)) Message = "Подтвердите пароль";
+
+            if (Password != ConfirmPassword) Message = "Пароли не совпадают";
+
+            try
+            {
+                var result = await _apiService.RegisterAsync(
+                    new RegisterDto
+                    {
+                        UserName = UserName,
+                        Password = Password
+                    });
+                if (result!.IsSuccess)
+                {
+                    Message = "Регистрация прошла успешно";
+                    await _navigationService.NavigateToLoginAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"[AuthViewModel]: {ex.Message}");
+                Message = "Ошибка регистрации";
+            }
+        }
+
+        [RelayCommand]
+        private void Reset()
+        {
+            UserName = string.Empty;
+            Password = string.Empty;
+            ConfirmPassword = string.Empty;
+            Message = string.Empty;
         }
     }
 }
