aa463dad8abe7c7d37de42f5d943bfae2b8d148a
GrinchenkoAnna <grinchenkoanna@inbox.ru>
2026-02-17 16:43:47 +0700

    Deleted old tables from database

---
+++ DietHelper.Common/Migrations/20260217093945_RemoveOldTables.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DietHelper.Common.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveOldTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DishIngredients",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "Dishes",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "Products",
+                schema: "public");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
